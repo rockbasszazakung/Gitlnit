d1b0127f7844b85a3b47a5b6a05b2005dba66018
rockbasszazakung <rockbaszazakung@gmail.com>
2018-10-18 11:51:04 +0700

    hello

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,22 @@
 
 public class Hello {
+	String name;
+
+	public Hello(String name) {
+		super();
+	this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String gethello(){
+		return "Hello" + this.name;
+	}
 	
 }
