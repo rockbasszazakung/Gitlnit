b8d07b7412e6d8a6457266d511a70dc20a45daa8
moonpingza123 <moonpingza@outlook.com>
2018-10-18 11:58:08 +0700

    update file main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
 
 public class Main {
-
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String name = in.next();
+		Hello hello = new Hello(name);
+		System.out.println(hello.gethello());
+	}
 }
